2b865277ba6ec82fbc387350353a7bb9bbad859d
Rahul Goyal <rg6393@gmail.com>
2020-02-26 15:41:47 +0530

    add to repo

---
+++ Sq_func.py
@@ -0,0 +1,18 @@
+def foo(x):  # x is a function parameter
+    print("x = " + str(x))
+
+
+foo(5)  # pass 5 to foo(). Here 5 is an argument passed to function foo.
+
+
+def square(x):
+    print(x ** 2)
+
+def square(y): # this will be excuted only
+    print(y/2)
+
+square(4)
+square(8)
+square(15)
+square(23)
+square(42)
