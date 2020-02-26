e1c8f3ced3f334329e84f8f274258600f1bb07ac
Rahul Goyal <rg6393@gmail.com>
2020-02-26 08:38:46 +0530

    second commit

--- loops.py
+++ loops.py
@@ -1,1 +1,19 @@
+import random, secrets
+
 print('hello')
+count = 0
+
+while True:  # this condition cannot possibly be false
+    print(count)
+    count += 1
+    if count >= 5:
+        break           # exit loop if count >= 5
+
+
+zoo = ["lion", 'tiger', 'elephant']
+while True:                         # this condition cannot possibly be false
+    animal = secrets.choice(zoo)
+    #another_animal = zoo.pop()       # extract one element from the list end
+    print(animal)
+    if animal == 'elephant':
+        break           # exit loop
