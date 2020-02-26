a30af392dd2bab6998cad33b4b6f078b999b8c04
Rahul Goyal <rg6393@gmail.com>
2020-02-26 08:47:53 +0530

    Initial Commit

---
+++ random_choices.py
@@ -0,0 +1,11 @@
+import random
+# Random module helps in selecting a random item from a set
+# of data.
+# random.choice(population, k) where first argument is
+# dataset and k is size
+# random.sample(population, k) where first argument is
+# dataset and k is size. here k cannot be greater than
+# size of dataset
+list = [20, 30, 40, 50 ,60, 70, 80]
+sampling = random.sample(list, k=5)
+print(sampling)
