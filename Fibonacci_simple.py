9b54e9735c329a13417ddb2d6653fa346e8031b2
Rahul Goyal <rg6393@gmail.com>
2020-02-27 17:50:42 +0530

    test code

--- Fibonacci_simple.py
+++ Fibonacci_simple.py
@@ -1,8 +1,13 @@
 number = input("No. of terms to be shown in the Fibonacci Series:")
-
+result= []
+i = 0
 temp = 0
-a = 0
-b = 1
+first = 0
+scnd = 1
 
-while i < number:
-    
+while i < int(number):
+    print(first)
+    temp = scnd
+    first = temp
+    scnd = scnd + first
+    i += 1
