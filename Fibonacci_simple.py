25c7297fbd4729ac9c8d90a309061fbfdeb6b2a5
Rahul Goyal <rg6393@gmail.com>
2020-02-28 12:53:06 +0530

    Completed Fibonacci Self

--- Fibonacci_simple.py
+++ Fibonacci_simple.py
@@ -1,13 +1,21 @@
-number = input("No. of terms to be shown in the Fibonacci Series:")
-result= []
-i = 0
-temp = 0
-first = 0
-scnd = 1
 
-while i < int(number):
-    print(first)
-    temp = scnd
-    first = temp
-    scnd = scnd + first
-    i += 1
+n = input("No. of terms to be shown in the Fibonacci Series:")
+n = int(n)
+print('Below are the Fibonacci series upto %d terms.' % n)
+def fibonacci(number):
+
+    result = []
+    i = 0
+    temp = 0
+    first = 0
+    scnd = 1
+
+    while i < int(number):
+        result.append(first)
+        temp  = scnd
+        scnd  = scnd + first
+        first = temp
+        i += 1
+    return result
+
+print(fibonacci(n))
