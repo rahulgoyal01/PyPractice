27d39a885ea00660a20d6011c8cb2eb0621a544c
Rahul Goyal <rg6393@gmail.com>
2020-03-14 23:40:31 +0530

    new practice from udemy

--- ATBS Udemy/Fortun_teller_v2.py
+++ ATBS Udemy/Fortun_teller_v2.py
@@ -1,7 +1,7 @@
 import random
 
 class Fortune:
-    random_no=random.randint(1, 9)
+
     # check how to use these variables from init
     def  __init__(self, name, color, age):
         self.name = name
@@ -9,12 +9,12 @@ class Fortune:
         self.age = age
 
     @classmethod
-    def from_input(cls):
-        return cls(
-            input('Name: '),
-            input('Color: '),
-            int(input('Age: ')),
-        )
+    def from_input(self):
+        name = input('Name: ')
+        color = input('Color: ')
+        age = int(input('Age: '))
+        return self(name,color,age)
+
     @staticmethod
     def random_fortune():
         random_no = random.randint(1, 9)
@@ -24,5 +24,10 @@ class Fortune:
             print("Bad Luck mate")
 
 
+    def greeting(self, name):
+        return "Hi" + name
+
+
 user = Fortune.from_input()
+Fortune.greeting("Rahul")
 Fortune.random_fortune()
