b2ab08f5024e7c681ed5ea69cb9bdaebb1516661
Rahul Goyal <rg6393@gmail.com>
2020-03-19 00:27:00 +0530

    Dictionary example file

---
+++ ATBS Udemy/Lessons practice/Dictionary.py
@@ -0,0 +1,11 @@
+
+myDict = {'key1': 'Value1', 'key2': 'Value2',
+           'key3': 'Value3'}
+
+print("this is  " + myDict['key2'])
+
+#dictionaries are mutable like list
+#dict supports list(), value() and item() methods.
+# item method returns dict key and value as tuple.
+
+print(myDict.items())
