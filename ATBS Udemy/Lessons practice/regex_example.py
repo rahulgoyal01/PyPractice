a929c2b07ea95b6c7b360811e35df1c74c289f35
Rahul Goyal <rg6393@gmail.com>
2020-03-22 02:01:38 +0530

    regex

--- ATBS Udemy/Lessons practice/regex_example.py
+++ ATBS Udemy/Lessons practice/regex_example.py
@@ -18,3 +18,13 @@ def isPhoneNumber(text):
     return True
 
 print(isPhoneNumber('999-345-7865'))
+
+msg = "Call me on 426-847-9898 or my personal cell 222-909-9999"
+foundNumber = False
+for i in range(len(msg)):
+    chunk = msg[i:i + 12]
+    if isPhoneNumber(chunk):
+        print("Phone number found: " + chunk)
+        foundNumber = True
+if not foundNumber:
+    print("Counld not find the number.")
---
+++ ATBS Udemy/Lessons practice/regex_reModule.py
@@ -0,0 +1,28 @@
+import re
+
+message = "Call me on 426-847-9898 or my personal cell 222-909-9999"
+
+phnRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo = phnRegex.search(message)
+print(mo.group()) #match object
+
+#search method will look into the string for the
+#specified pattern
+
+#Compile function creates a regex object.
+
+#group method, ties the finding as a single object
+# use of parenthesis will divide then into groups
+
+grpRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+num = grpRegex.search(message)
+print(num.group(1)) # prints the area code
+print(num.group(2))
+
+
+phnRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+mo1 = phnRegex.findall(message)
+print(mo1)
+
+# findall returns all the number in a string
+# in a list
