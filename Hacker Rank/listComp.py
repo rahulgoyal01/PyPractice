6ae36d7c64108ed686b7457569405f254bedab9d
Rahul Goyal <rg6393@gmail.com>
2020-04-01 17:48:20 +0530

    Update listComp.py

--- Hacker Rank/listComp.py
+++ Hacker Rank/listComp.py
@@ -1,6 +1,10 @@
-x,y,z,n = [input() for i in range(4)]
-print([a,b,c]
+x,y,z,n = [int(input()) for i in range(4)]
+
+list = [[a,b,c]
         for a in range(x+1)
         for b in range(y+1)
         for c in range(z+1)
-        if a+b+c != n)
+        if a+b+c != n]
+print(list)
+
+#def list_comp(x, y, z , n):
